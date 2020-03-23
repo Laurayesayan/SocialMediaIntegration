6c8c61d9c4ee6c456613f7a786314fb3136df8dc
laurayesayan <laurayesayan@gmail.com>
2020-03-23 23:38:42 +0300

    Added link sharing for facebook.

--- SocialMediaIntegration_2_9/ViewController.swift
+++ SocialMediaIntegration_2_9/ViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 import FacebookLogin
+import FacebookShare
+import FBSDKShareKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SharingDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -58,8 +60,31 @@ class ViewController: UIViewController {
         }
     }
     
+    func shareLink(url: String) {
+        let shareContent = ShareLinkContent()
+        shareContent.contentURL = URL.init(string: url)!
+        shareContent.quote = "Test share from my app"
+        ShareDialog.init(fromViewController: self, content: shareContent, delegate: self).show()
+        
+    }
+
+    func sharer(_ sharer: Sharing, didCompleteWithResults results: [String : Any]) {
+        if sharer.shareContent.pageID != nil {
+            print("Share: Success")
+        }
+    }
+    func sharer(_ sharer: Sharing, didFailWithError error: Error) {
+        print("Share: Fail")
+    }
+    func sharerDidCancel(_ sharer: Sharing) {
+        print("Share: Cancel")
+    }
+    
+    @IBAction func facebookShare(_ sender: Any) {
+        shareLink(url: "https://github.com/facebookarchive/facebook-swift-sdk/blob/master/Samples/Catalog/Sources/ShareDialogViewController.swift")
+    }
     
 
-
 }
+
 
